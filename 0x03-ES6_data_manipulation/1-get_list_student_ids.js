a495ca21d8a8a1f268fe8e91dac4ad38ca97cc83
anto-muli <memuli@students@uonbi.ac.ke>
2024-06-04 05:52:15 +0300

    Done

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,10 +1,4 @@
 export default function getListStudentIds(list) {
-    if (Array.isArray(list)) {
-      return list.map((obj) => obj.id);
-    }
-    return [];
-  }
-export default function getListStudentIds(list) {
   if (Array.isArray(list)) {
     return list.map((obj) => obj.id);
   }
